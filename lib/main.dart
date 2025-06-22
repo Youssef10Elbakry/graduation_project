2cb02cc38ad644634ac8994a31a1e8760da5c167
youssuf <y.mahmoud2145@nu.edu.eg>
2025-06-22 17:49:55 +0200

    Resolve conflict in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+
 import 'package:graduation_project/ui/providers/home_tab_provider.dart';
 import 'package:graduation_project/ui/providers/sign_in_button_provider.dart';
 import 'package:graduation_project/ui/providers/student_profile_tab_bar_provider.dart';
@@ -9,10 +10,7 @@ import 'package:graduation_project/ui/providers/wallet_tab_provider.dart';
 import 'package:graduation_project/ui/providers/passcode_provider.dart';
 import 'package:graduation_project/ui/providers/comfirmation_provider.dart';
 
-import 'package:graduation_project/ui/screens/attendence_screen/attendance_screen.dart';
-import 'package:graduation_project/ui/screens/grades_screen/grades_screen.dart';
 import 'package:graduation_project/ui/screens/splash_screen.dart';
-import 'package:graduation_project/ui/screens/transactions_details/transactions_details.dart';
 import 'package:graduation_project/ui/screens/welcome_screen/welcome_screen.dart';
 import 'package:graduation_project/ui/screens/login_screen/login_screen.dart';
 import 'package:graduation_project/ui/screens/main_screen/main_screen.dart';
@@ -24,6 +22,9 @@ import 'package:graduation_project/ui/screens/passcode_screen/passcode_screen.da
 import 'package:graduation_project/ui/screens/passcode_screen/success_screen.dart';
 import 'package:graduation_project/ui/screens/forgot_password_screens/forgot_password_screen.dart';
 import 'package:graduation_project/ui/screens/forgot_password_screens/successful_screen.dart';
+import 'package:graduation_project/ui/screens/transactions_details/transactions_details.dart';
+import 'package:graduation_project/ui/screens/attendence_screen/attendance_screen.dart';
+import 'package:graduation_project/ui/screens/grades_screen/grades_screen.dart';
 import 'package:graduation_project/ui/screens/main_screen/profile_tab/profile_tab.dart';
 
 void main() {
@@ -77,7 +78,7 @@ class MyApp extends StatelessWidget {
         SuccessfulScreen.routeName: (_) => const SuccessfulScreen(),
         TransactionsDetailsScreen.routeName: (_) => const TransactionsDetailsScreen(),
         GradesScreen.routeName: (_) => const GradesScreen(),
-        ProfileTab.screenName: (_) =>  ProfileTab(),
+        ProfileTab.screenName: (_) => ProfileTab(),
       },
       initialRoute: MainScreen.screenName,
     );
